9d3b9f6ab8991fb3ba092dd4c5b20201bc64a346
Marc Peabody <marc@edgecase.com>
2012-02-07 16:57:59 -0500

    add SRM glass to Ember

--- app/assets/javascripts/quizzes.js
+++ app/assets/javascripts/quizzes.js
@@ -1,6 +1,5 @@
 $(function(){
   //$('.srm_range[title]').qtip();
-
   _.randomIndex = function(arr){
     return Math.floor(Math.random() * arr.length);
   }
@@ -53,4 +52,26 @@ $(function(){
   window.append = function(emberView){
     emberView.appendTo('.mainArea');
   }
+
+  window.srm_colors = {0: '#FFFFFF',
+     1: '#F3F993', 2: '#F5F75C', 3: '#F6F513', 4: '#EAE615', 5: '#E0D01B',
+     6: '#D5BC26', 7: '#CDAA37', 8: '#C1963C', 9: '#BE8C3A', 10: '#BE823A',
+    11: '#C17A37', 12: '#BF7138', 13: '#BC6733', 14: '#B26033', 15: '#A85839',
+    16: '#985336', 17: '#8D4C32', 18: '#7C452D', 19: '#6B3A1E', 20: '#5D341A',
+    21: '#4E2A0C', 22: '#4A2727', 23: '#361F1B', 24: '#261716', 25: '#231716',
+    26: '#19100F', 27: '#16100F', 28: '#120D0C', 29: '#100B0A', 30: '#050B0A' };
+  window.srmRGB = function(colorNumber){
+    colorNumber = parseInt(colorNumber);
+    if(!colorNumber || colorNumber > 30) colorNumber = 30;
+    return srm_colors[colorNumber];
+  }
+  Handlebars.registerHelper('glass', function(property) {
+    var value = Ember.getPath(this, property);
+    var rgb = srmRGB(value);
+return new Handlebars.SafeString("<div class='srm_range' style='background-color: "+
+    rgb +
+    "; width: 25px; height: 25px; display: inline-block;'> <img src='/images/glass.png' width='100px' height='100px' alt='"+
+    value +
+    "SRM'/> </div>");
+  });
 });
