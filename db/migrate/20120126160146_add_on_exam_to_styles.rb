ac33e3e17838fb39684e3af0928528c44e8bcf36
Marc Peabody <marc@edgecase.com>
2012-02-07 16:58:10 -0500

    Belgian Specialty not on exam

--- db/migrate/20120126160146_add_on_exam_to_styles.rb
+++ db/migrate/20120126160146_add_on_exam_to_styles.rb
@@ -1,7 +1,7 @@
 class AddOnExamToStyles < ActiveRecord::Migration
   def up
     add_column :styles, :on_exam, :boolean, :default => true
-    styles_not_on_exam = %w{20A 21A 21B 22A 22B 22C 23A}
+    styles_not_on_exam = %w{16E 20A 21A 21B 22A 22B 22C 23A}
     ActiveRecord::Base.connection.execute("UPDATE styles SET on_exam=false WHERE style_number in (#{styles_not_on_exam.map{|s| "'#{s}'" }.join(',')})")
     add_index :styles, :on_exam
   end
